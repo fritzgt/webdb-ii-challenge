a45871975e62b86236ecfc22367875c7a266f113
fritzgt <tuanis@live.com>
2019-08-09 11:16:14 -0400

    created knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,14 @@
 
 module.exports = {
   development: {
+    // our DBMS driver
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      // the location of our db
+      filename: './data/car-dealer.db3'
+    },
+    // necessary when using sqlite3
+    useNullAsDefault: true
   }
 };
+//Deleted the rest of the code below which was not necesary
